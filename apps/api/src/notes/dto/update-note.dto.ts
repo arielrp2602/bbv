bf49319838388829027d8b132d6fa4bf5a5e2569
Ariel Ramirez <arielrp2602@gmail.com>
2026-04-18 22:28:07 -0600

    feat: add trackingNumber to Note

--- apps/api/src/notes/dto/update-note.dto.ts
+++ apps/api/src/notes/dto/update-note.dto.ts
@@ -1,8 +1,12 @@
 import { NoteType } from '@prisma/client';
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class UpdateNoteDto {
   @IsOptional()
   @IsEnum(NoteType)
   type?: NoteType;
+
+  @IsOptional()
+  @IsString()
+  trackingNumber?: string;
 }
